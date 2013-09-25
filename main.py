543af1f31c7eb055ff1109f4331f3893f6990743
KS Chan <mrkschan@gmail.com>
2013-09-25 17:59:44 +0800

    Included basic fetching ability

---
+++ main.py
@@ -0,0 +1,45 @@
+import getpass
+import sys
+
+import gmail  # github.com/charlierguo/gmail
+
+try:
+    import argparse
+except:
+    # argparse is only available in Python 2.7+
+    print >> sys.stderr, 'pip install -U argparse'
+    sys.exit(1)
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('username', type=str, help='Gmail account')
+    argparser.add_argument('-p', type=str, dest='password',
+                           help='Gmail account password')
+    argparser.add_argument('-l', '--label', type=str,
+                           help='Filter email by label')
+    argparser.add_argument('-c', '--content', type=str,
+                           help='Filter email by content specified')
+    args = argparser.parse_args()
+
+    username = args.username
+    password = args.password or getpass.getpass()
+
+    mbox = gmail.login(username, password)
+
+    if args.label:
+        mbox = mbox.label(args.label)
+        if not mbox:
+            print >> sys.stderr, 'Specified label is not available.'
+            sys.exit(1)
+
+    for email in mbox.mail():
+        email.fetch()
+        if args.content and args.content not in email.body:
+            continue
+
+        print email.subject
+
+
+if __name__ == '__main__':
+    main()
